325559d005f35952c8132e874cba4f9b7359fea9
Adam Calhoun <a.t.calhoun@gmail.com>
2018-02-09 10:15:00 -0800

    test compareDate

--- src/age.js
+++ src/age.js
@@ -2,8 +2,12 @@ import { time } from './times.js';
 
 export function yearToSeconds(years){
   let result = years * time.year ;
-  result *= time.day;
+  result *= time.day; //almost certainly a more elegant way to write this, but written for clarity
   result *= time.hour;
   result *= time.minute;
   return result
+}
+
+export function compareDate(dayOne, dayTwo){
+
 }
--- test/age.test.js
+++ test/age.test.js
@@ -11,7 +11,15 @@ describe( 'Age', function() {
   });
 
 //take two dates and compare the difference
-
+  describe('compareDate', function(){
+    it("should calculate the difference in seconds between two dates", function(){
+      var dayOne = new Date(2018, 0, 27);
+      var dayTwo = new Date(2018, 1, 6);
+      // dayOne.setFullYear(2018).setMonth(0).setDate(27);
+      // dayTwo.setFullYear(2018).setMonth(1).setDate(6);
+      assert.equal(950400, Age.compareDate(dayOne, dayTwo));
+    })
+  })
 //return age in Mercury years (.24 earth years)
 
 //return age in Venus years (.62 earth years)
