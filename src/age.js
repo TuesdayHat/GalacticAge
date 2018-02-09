bc5db805bcf2bb438d2e9a7bfc79b90e9c955826
Guest User <Guest@Epicodus-4C.local>
2018-02-09 08:59:49 -0800

    create framework for testing, file structure

---
+++ src/age.js
@@ -0,0 +1,8 @@
+import * as Age from '../src/age.js';
+const assert = require( 'assert' );
+
+describe( 'Age', function() {
+
+
+
+})
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import * as Age from './age.js';
+
+
+$(document).ready(function(){
+
+});
